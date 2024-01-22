92dd4501be958ad4fdc3ad607d2f4f8ce56a190c
Apostol Savov <apostol.savov.97@gmail.com>
2024-01-22 18:41:26 +0200

    comments

--- client/src/app/diaryEntries/ViewEntries/DiaryRoute.tsx
+++ client/src/app/diaryEntries/ViewEntries/DiaryRoute.tsx
@@ -1,39 +1,40 @@
-import Timeline from '@mui/lab/Timeline'
-import TimelineItem from '@mui/lab/TimelineItem'
-import TimelineSeparator from '@mui/lab/TimelineSeparator'
-import TimelineConnector from '@mui/lab/TimelineConnector'
-import TimelineContent from '@mui/lab/TimelineContent'
-import TimelineDot from '@mui/lab/TimelineDot'
-import Button from 'app/common/components/Button'
+// import Timeline from '@mui/lab/Timeline'
+// import TimelineItem from '@mui/lab/TimelineItem'
+// import TimelineSeparator from '@mui/lab/TimelineSeparator'
+// import TimelineConnector from '@mui/lab/TimelineConnector'
+// import TimelineContent from '@mui/lab/TimelineContent'
+// import TimelineDot from '@mui/lab/TimelineDot'
+// import Button from 'app/common/components/Button'
 
 //testing
 export default function BasicTimeline() {
   return (
-    <Timeline>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Button>
-            Testing
-          </Button>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Code</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-        </TimelineSeparator>
-        <TimelineContent>Sleep</TimelineContent>
-      </TimelineItem>
-    </Timeline>
+    <></>
+    // <Timeline>
+    //   <TimelineItem>
+    //     <TimelineSeparator>
+    //       <TimelineDot />
+    //       <TimelineConnector />
+    //     </TimelineSeparator>
+    //     <TimelineContent>
+    //       <Button>
+    //         Testing
+    //       </Button>
+    //     </TimelineContent>
+    //   </TimelineItem>
+    //   <TimelineItem>
+    //     <TimelineSeparator>
+    //       <TimelineDot />
+    //       <TimelineConnector />
+    //     </TimelineSeparator>
+    //     <TimelineContent>Code</TimelineContent>
+    //   </TimelineItem>
+    //   <TimelineItem>
+    //     <TimelineSeparator>
+    //       <TimelineDot />
+    //     </TimelineSeparator>
+    //     <TimelineContent>Sleep</TimelineContent>
+    //   </TimelineItem>
+    // </Timeline>
   )
 }
